4af7003aa10c09936ad4072c308455f9ec3afb55
JatMi <2224752336@qq.com>
2016-03-29 11:52:10 +0800

    modified:   css/m/base.css 	modified:   css/m/register.css 	modified:   css/pc/register.css 	new file:   js/common.js 	modified:   login.html 	modified:   register.html

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function(){
+   /* $('.content input').focus(function () {
+        var text=$(this).attr('value');
+        $(this).attr('value','');
+        console.log(text);
+        $(this).blur(function(){
+            if(!$(this).attr('value')){
+                $(this).attr('value',text);
+            }
+        })
+    });*/
+
+});
